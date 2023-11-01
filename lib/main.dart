01a05e9048faa724beb3389598f3d178cb06278d
HuzaifaJamal <huzaifajamal95@gmail.com>
2023-11-01 14:09:58 +0500

    Update Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,25 @@ import 'package:flutter/material.dart';
 
 import 'ui.dart';
 
+void main()  {
+  runApp(MyApp());
+}
 
-
-
-void main() async {
-
+class MyApp extends StatelessWidget {
   final googleSheetsApi = GoogleSheetsApi();
-  // await googleSheetsApi.init();
-  
-  runApp(MaterialApp(
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: Text('Google Sheets Data Entry App'),
       ),
       body: MyUI(googleSheetsApi: googleSheetsApi),
     ),
-  ));
+  );
+  }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-       
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: MyUI(googleSheetsApi: googleSheetsApi),
-//     );
-//   }
-// }
 
